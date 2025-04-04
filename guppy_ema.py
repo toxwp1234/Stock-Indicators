55b2d5aaabc3f79952748843ee3b89577fc8b866
toxwp1234 <czescjakczesc@gmail.com>
2025-04-04 19:12:08 +0200

    Cleaned Up

---
+++ guppy_ema.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from EMA_custom  import EMA
+
+
+### Funkcja Guppy EMA generuje średnią ważoną kroczacą dla każdej wartości z listy
+
+def guppy_mode_ema(price_arr:list ,arr_of_peroid:list):
+
+    ###     price_arr -- ceny
+    ###     arr_of_peroid -- lista wszytkich okresów do guppy
+
+    for element in arr_of_peroid :
+        ema_string : tuple = EMA(price_arr,element)
+        plt.plot(list(ema_string.keys()),list(ema_string.values()),label="EMA") 
